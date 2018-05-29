50dcc29f9b155b914f406b78e79a9d74d40cc835
Changmin Lim <chang.lim92@gmail.com>
2018-05-29 00:09:49 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0 
  myresult = []
  while i < collection.length 
-  result = yield(collection[])
+  result = yield(collection[i])
   if result == TRUE 
     myresult.push(collection[i])
   end 
